4234c0b9901a36d5407475617fa434019d502b0c
Sainadh1245 <sainadhdanduprolu@gmail.com>
2026-09-07 05:48:36 -0400

    Update application message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,8 @@
-package com.example;
+
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Java Application!");
+        System.out.println("hello from jenkins automatic built!");
         System.out.println("Running on Kali Linux");
     }
 }
